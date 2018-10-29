24ed2afd8155a82e8defdad4e6dec8763a933a61
Muhamad Risman <real.risman@gmail.com>
2018-10-29 19:26:36 +0700

    Add var ErrSlotAlreadyAllocated on perror package

--- src/perror/error.go
+++ src/perror/error.go
@@ -5,6 +5,7 @@ import "errors"
 
 var (
 	ErrSlotNumberInvalid        = errors.New("slot: Please provide valid slot number 1 or greater")
+	ErrSlotAlreadyAllocated     = errors.New("slot: Slots already allocated")
 	ErrParkingFullCapacity      = errors.New("Sorry, parking lot is full")
 	ErrVehicleAssignInvalidSlot = errors.New("vehicle: Cannot allocate vehicle in invalid slot")
 )
