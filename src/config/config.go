83f670ce06086e6656b491b5eb374301ce5a1dcf
Muhamad Risman <real.risman@gmail.com>
2018-10-29 19:51:05 +0700

    Add global configuration package

---
+++ src/config/config.go
@@ -0,0 +1,13 @@
+// Package config stores global applcation configurations
+package config
+
+import (
+	"ptypes"
+)
+
+const (
+	// Capacity Parking default value
+	Capacity = ptypes.Capacity(0)
+	// Start : Slot start index in Parking default value
+	Start = ptypes.Index(1)
+)
