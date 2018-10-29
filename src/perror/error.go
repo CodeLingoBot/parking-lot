2f965672242952a35e217054632bfd5667f949c8
Muhamad Risman <real.risman@gmail.com>
2018-10-29 19:03:34 +0700

    Add ErrParkingFullCapacity on perror package

--- src/perror/error.go
+++ src/perror/error.go
@@ -4,5 +4,6 @@ package perror
 import "errors"
 
 var (
-	ErrSlotNumberInvalid = errors.New("slot: Please provide valid slot number 1 or greater")
+	ErrSlotNumberInvalid   = errors.New("slot: Please provide valid slot number 1 or greater")
+	ErrParkingFullCapacity = errors.New("Sorry, parking lot is full")
 )
