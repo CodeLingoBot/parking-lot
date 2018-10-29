8600cc89576bd7b2f9e6500f17237904d42015d1
Muhamad Risman <real.risman@gmail.com>
2018-10-29 20:42:39 +0700

    Add unit test for vehicle package

---
+++ src/vehicle/vehicle_test.go
@@ -0,0 +1,28 @@
+package vehicle
+
+import "testing"
+
+var vehicles = []Vehicle{
+	{"", ""},
+	{"XXX", "CCC"},
+}
+
+func TestVehicle_GetColour(t *testing.T) {
+	var v *Vehicle
+	for _, o := range vehicles {
+		v = New(o.Number, o.Color)
+		if v.GetColour() != o.Color {
+			t.Error("Colour should be shame")
+		}
+	}
+}
+
+func TestVehicle_GetNumber(t *testing.T) {
+	var v *Vehicle
+	for _, o := range vehicles {
+		v = New(o.Number, o.Color)
+		if v.GetNumber() != o.Number {
+			t.Error("Number should be shame")
+		}
+	}
+}
