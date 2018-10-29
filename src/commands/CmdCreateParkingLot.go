26e26247db13d69a96a050e1022b2fd2aacb8852
Muhamad Risman <real.risman@gmail.com>
2018-10-30 01:17:59 +0700

    Add struct CmdCreateParkingLot

---
+++ src/commands/CmdCreateParkingLot.go
@@ -0,0 +1,60 @@
+// Package commands with 'create_parking_lot' command implementation
+package commands
+
+import (
+	"config"
+	"fmt"
+	"parking"
+	"perror"
+	"ptypes"
+	"store"
+	"strconv"
+)
+
+// CmdCreateParkingLot defined arguments and related methods
+type CmdCreateParkingLot struct {
+	Command
+	Capacity ptypes.Capacity
+}
+
+// NewCmdCreateParkingLot new command instance
+func NewCmdCreateParkingLot() *CmdCreateParkingLot {
+	var cmd = new(CmdCreateParkingLot)
+	cmd.Cmd = "create_parking_lot"
+	return cmd
+}
+
+// Help to print help of 'create_parking_lot' command
+func (ccp *CmdCreateParkingLot) Help() string {
+	return `create_parking_lot <slots count>
+	Create parking lot slots.
+	Eg: create_parking_lot 6`
+}
+
+// Parse to parse arguments
+func (ccp *CmdCreateParkingLot) Parse(argString string) error {
+	ccp.Command.Parse(argString)
+	if perror.Empty != ccp.Args[0] {
+		val, err := strconv.ParseUint(ccp.Args[0], 0, 64)
+		if nil != err {
+			return perror.ErrInvalidParams
+		}
+		ccp.Capacity = ptypes.Capacity(val)
+	}
+	return nil
+}
+
+// Run to execute the command and provide result
+func (ccp *CmdCreateParkingLot) Run() (string, error) {
+	pc := parking.New(config.Start, ccp.Capacity)
+	if nil != pc {
+		store.Get().SetParkingCenter(pc)
+		ccp.OutPut = fmt.Sprintf(
+			"Created a parking lot with %v slots",
+			ccp.Capacity,
+		)
+	} else {
+		ccp.OutPut = perror.ErrCreateParkingCenter.Error()
+	}
+	return ccp.OutPut, nil
+}
