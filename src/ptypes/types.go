645ddae94ceece91b7dfa7523b8a9b42183b3837
Muhamad Risman <real.risman@gmail.com>
2018-10-29 18:09:28 +0700

    Recommit Create specific object of types

---
+++ src/ptypes/types.go
@@ -0,0 +1,8 @@
+// Package ptypes keep the solution spesific types
+package ptypes
+
+// Capacity of parking center data type
+type Capacity uint64
+
+// Index is slot index number type
+type Index uint64
