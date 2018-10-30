a33f2dba2f627abca296d97d27abadfc8957bf14
Muhamad Risman <real.risman@gmail.com>
2018-10-30 16:06:29 +0700

    Add package commands with 'slot_numbers_for_cars_with_colour'

---
+++ src/commands/CmdGetSlotNumWithColour.go
@@ -0,0 +1,65 @@
+// Package commands with 'slot_numbers_for_cars_with_colour'
+// command implementation
+package commands
+
+import (
+	"strings"
+	"fmt"
+	"perror"
+)
+
+// CmdGetSlotNumWithColour defined arguments and related methods
+type CmdGetSlotNumWithColour struct {
+	Command
+	Color string
+}
+
+// NewCmdGetSlotNumWithColour new command instance
+func NewCmdGetSlotNumWithColour() *CmdGetSlotNumWithColour {
+	var cmd = new(CmdGetSlotNumWithColour)
+	cmd.Cmd = "slot_numbers_for_cars_with_colour"
+	return cmd
+}
+
+// Help to print command help information
+func (cgc *CmdGetSlotNumWithColour) Help() string {
+	return `slot_numbers_for_cars_with_colour <colour>
+	Fetch all slot numbers of cars with known colour
+	Eg: slot_numbers_for_cars_with_colour White`
+}
+
+// Parse to parse arguments
+func (cgc *CmdGetSlotNumWithColour) Parse(argString string) error {
+	cgc.Command.Parse(argString)
+	cgc.Color = cgc.Args[0]
+	return nil
+}
+
+// Verify to check the provided parameters are valid or not
+func (cgc *CmdGetSlotNumWithColour) Verify() error {
+	if perror.Empty == cgc.Color {
+		return perror.ErrInvalidParams
+	}
+	return nil
+}
+
+// Run to execute the command and provide result
+func (cgc *CmdGetSlotNumWithColour) Run() (string, error) {
+	var outPutList = []string{}
+	pC := store.Get().GetParkingCenter()
+	slots, err := pC.ReportVehicleByColor(cgc.Color)
+	if nil == err {
+		for _, s := range slots {
+			outPutList = append(
+				outPutList,
+				fmt.Sprintf("%v", s.GetNumber()),
+			)
+		}
+	} else {
+		outPutList = []string{
+			err.Error()
+		}
+	}
+	cgc.OutPut = strings.Join(outPutList, perror.Comma)
+	return cgc.OutPut, err
+}
