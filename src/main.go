f5dbf60d8071f012f358f0d1c1e667f896a5188b
Muhamad Risman <real.risman@gmail.com>
2018-10-30 18:28:21 +0700

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"commands"
+	"os"
+)
 
+func main() {
+	// Process the input file and give and interactive shell
+	if len(os.Args) > 1 && "" != os.Args[1] {
+		commands.NewFileCommandProcessor(os.Args[1]).Process()
+	}
+	// Interactice shell will provide to user to operate
+	// with file data and console process
+	commands.NewShell().Process()
 }
